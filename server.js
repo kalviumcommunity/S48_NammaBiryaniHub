7211c845c365e381fbaf32ad4b183781588315da
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-02-16 15:54:46 +0530

    created routes.js and created crud files in docs.bruno

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const routes = require('./routes');
 
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
 const mongoDbUri = process.env.MONGODB_URI;
 
 const client = new MongoClient(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+app.use(express.json());
+app.use('/api', routes);
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
